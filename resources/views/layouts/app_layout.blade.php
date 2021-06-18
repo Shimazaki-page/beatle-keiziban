3a3e37d0aefe73072faddd5ff113adb9f0062724
s-shimazaki <s-shimazaki@s-shimazakinoMacBook-Pro.local>
2021-06-18 17:42:09 +0900

    レイアウトにdescription追加

--- resources/views/layouts/app_layout.blade.php
+++ resources/views/layouts/app_layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') | {{config('app.name')}}</title>
+    <meta name="description" content="ユーザー匿名性掲示板">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="/js/app.js" defer></script>
